39ef494e2aee2428b5f03c40c6e2e8e68cea7174
Eduardo Reyes <eduarno.1@hotmail.com>
2021-01-25 21:42:26 -0600

    condicionales if

---
+++ if.js
@@ -0,0 +1,12 @@
+var edad = 18;
+
+if (edad === 18) {
+  console.log("Puedes votar, sera tu primera vez");
+} else if (edad > 18) {
+  console.log("puedes votar de nuevo");
+} else {
+  console.log("Aun no puedes votar");
+}
+
+var numero = 1;
+var resultado = numero === 1 ? "soy uno" : "no soy uno";
